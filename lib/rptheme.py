0a96278fc90d3b82b27c47dc70a0ae907bf79923
jamesWalker55 <30405220+jamesWalker55@users.noreply.github.com>
2023-11-22 18:31:46 +0800

    Allow adding extra INI configs from the command line

--- lib/rptheme.py
+++ lib/rptheme.py
@@ -4,11 +4,21 @@ from configparser import ConfigParser
 from lib import formatter
 
 
-def from_paths(paths: list[str]):
+def from_paths(paths: list[str], extra_configs: dict[str, dict[str, str]] = None):
+    if extra_configs is None:
+        extra_configs = {}
+
     config = ConfigParser()
 
     for p in paths:
         config.read(p)
+
+    # add extra configs
+    for section in extra_configs:
+        if section not in config:
+            config[section] = {}
+        for key in extra_configs[section]:
+            config[section][key] = extra_configs[section][key]
 
     # apply macros
     for section in config:
