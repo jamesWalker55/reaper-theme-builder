c7749bb5b71184cff898e606d3cb9842a7678101
James Walker <james.chunho@gmail.com>
2023-12-03 11:51:48 +0800

    unused scripts

--- scripts/file-diff-watcher/run.ps1
+++
@@ -1,5 +0,0 @@
-$python = "D:\Projects (Misc)\reaper-theme\scripts\file-diff-watcher\venv\Scripts\python.exe"
-$script = "D:\Projects (Misc)\reaper-theme\scripts\file-diff-watcher\diff-watch.py"
-$fileToWatch = "C:\Users\James\AppData\Roaming\REAPER\ColorThemes\temp.ReaperTheme"
-
-.$python $script $fileToWatch
