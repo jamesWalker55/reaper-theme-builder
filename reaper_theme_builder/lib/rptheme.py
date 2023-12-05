d4a8d5622a9f44b2bf0176bc256e3f9b8c6a9b32
James Walker <james.chunho@gmail.com>
2023-12-05 13:38:43 +0800

    remove formatting stuff from rptheme, i'm going to rewrite formatting stuff to a new module

--- reaper_theme_builder/lib/rptheme.py
+++ reaper_theme_builder/lib/rptheme.py
@@ -1,37 +1,13 @@
 from configparser import ConfigParser
 from io import StringIO
 
-from . import formatter
-from .presetcolors import ColorPresetConfig
 
-
-def from_paths(
-    paths: list[str],
-    extra_configs: dict[str, dict[str, str]] | None = None,
-    presetcolors: ColorPresetConfig | None = None,
-):
+def from_paths(paths: list[str]):
     """Load multiple *.ReaperTheme files and combine them into a single file"""
-    if extra_configs is None:
-        extra_configs = {}
-
     config = ConfigParser()
 
     for p in paths:
         config.read(p)
-
-    # add extra configs
-    for section in extra_configs:
-        if section not in config:
-            config[section] = {}
-        for key in extra_configs[section]:
-            config[section][key] = extra_configs[section][key]
-
-    # apply macros
-    for section in config:
-        for key in config[section]:
-            config[section][key] = formatter.parse(
-                config[section][key], presetcolors=presetcolors
-            )
 
     with StringIO() as f:
         config.write(f, space_around_delimiters=False)
